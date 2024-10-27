e7e898e5b6e87e4ec08ef500a90485da1403f92c
rohan bait <baitrohan1993@outlook.com>
2024-10-27 14:07:58 +0530

    test commit

--- TaskDemo/obj/Debug/net9.0/TaskDemo.AssemblyInfo.cs
+++ TaskDemo/obj/Debug/net9.0/TaskDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d221bfa4dd492492eace9dd51860717caca41d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
